3de63f0ae9c0179813ccf5a13c82f83fe7be6df8
Irshad Ahmad <luckyirsh786@gmail.com>
2023-11-14 17:20:36 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "home-desktop": "url('/assets/home/background-home-desktop.jpg')",
+        "home-mobile": "url('/assets/home/background-home-mobile.jpg')",
+        "home-tablet": "url('/assets/home/background-home-tablet.jpg')",
+
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        bellefair: ["Bellefair"],
+        BarlowCondensed: ["Barlow Condensed"],
+        Barlow: ["Barlow"],
+      },
+      colors: {
+        primary: "#0B0D17",
+        secondary: "#D0D6F9",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
